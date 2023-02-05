e6c1027fc967f03a930fb0a9720ac99ee8ced00f
Victor Sriqui <victorsriqui@gmail.com>
2023-02-04 19:02:30 -0700

    Add reassign, commit, and fixes

--- variables.js
+++ variables.js
@@ -1,16 +1,19 @@
-# Variables
-
-var name = "Victor Sriqui"
+var nameFull = "Victor Sriqui"
 var highSchool = "Walt Whitman"
 var favoriteMeal = "Shakshouka"
-
 var startingAge = 37
 var birthMonth = 11
 var birthYear = 1985
-var birthDay = 06
-
+var birthDay = 6
 var hasBeard = true
 var likesTomatoes = false
 var likesHbo = true
+
+nameFull = "Beverly Windershire"
+highSchool = "Hogwarts"
+favoriteMeal = "Anchovies"
+
+console.log(nameFull,highSchool,favoriteMeal,startingAge,birthMonth,birthYear,birthDay,hasBeard,likesTomatoes,likesHbo)
+
 
 
