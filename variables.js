dfb461a5ad2afce16723e5345789e718f07395f8
Victor Sriqui <victorsriqui@gmail.com>
2023-02-04 18:02:46 -0700

    Add numbers

--- variables.js
+++ variables.js
@@ -4,3 +4,9 @@ var name  = "Victor Sriqui"
 var highSchool = "Walt Whitman"
 var favoriteMeal = "Shakshouka"
 
+var startingAge = 37
+var birthMonth = 11
+var birthYear = 1985
+var birthDay = 06
+
+
